ecb98c215c28c20ad123f0a663a756b2d52e04da
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-08-27 14:03:49 +0300

    Freeze Object t

--- freezeConstObj.js
+++ freezeConstObj.js
@@ -1,10 +1,26 @@
+// function freezeObject(obj) {
+//     Object.freeze(obj);
+// }
+
+// const myObj = {
+//     name: "John",
+//     age: 30
+// };
+
+// freezeObject(myObj);
 function freezeObject(obj) {
-    Object.freeze(obj);
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+
+    Object.freeze(MATH_CONSTANTS);
+
+    try {
+        MATH_CONSTANTS.PI = 3.14159;
+    } catch (ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
 }
-
-const myObj = {
-    name: "John",
-    age: 30
-};
-
-freezeObject(myObj);
+const PI = freezeObject();
+console.log(PI);
