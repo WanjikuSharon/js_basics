02a0fe9947e08689a584fa010570d7ffcc469afc
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-09-01 12:51:32 +0300

    Template Literals

---
+++ tempateliterals.js
@@ -0,0 +1,10 @@
+firstName = 'John';
+secondName = 'Gashia';
+
+let fullname = `${firstName} ${secondName}`;//in one line
+
+let fullName = `${firstName} 
+${secondName}`;//in multi line
+
+console.log(fullName);
+console.log(fullname);
