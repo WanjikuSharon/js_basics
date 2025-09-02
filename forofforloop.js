cef5753b05ed91e46c2fd56286288da400519def
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-09-02 09:59:44 +0300

    Add comments to clarify income logging and total calculation

--- forofforloop.js
+++ forofforloop.js
@@ -3,7 +3,9 @@ const incomes = [1000, 2000, 3000, 4000, 5000]
 total = 0;
 
 for (income of incomes) {
+    //print out individual income
     console.log(income);
   total += income;
 }
+//print out the total
 console.log(total);
