d47a112ff8688e4d644854095263634bdd725968
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-09-04 10:29:47 +0300

    Add destructuringOps.js file with destructuring examples and functions

---
+++ BeginnerJS/destructuringOps.js
@@ -0,0 +1,41 @@
+var voxel = { x: 3.5, y: 1.2, z: 5.1 };
+
+const { x: a, y: b, z: c } = voxel;
+
+//Get temperature
+const AVG_TEMPERATURE = {
+    today: 77.0,
+    tomorrow: 79.5
+};//Declared variable
+
+function getTempOfTmrw(avgTemperatures) {
+    "use strict";
+    const { tomorrow: tempOfTomorrow } = avgTemperatures;//deconstructing operator
+    return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURE)); // Outputs: 79.5
+
+
+//nested objects
+const LOCAL_FORECAST = {
+    today: { min: 72, max: 83 },
+    tomorrow: { min: 75, max: 88 }
+};
+
+function getAvgTemp(forecast) {
+    "use strict";
+
+    const { tomorrow: { max: getAvgTemp } } = forecast;
+    return getAvgTemp;
+}
+
+//Array destructuring
+const [firstName, lastName, middleName] = ['John', 'Doe', 'Smith'];
+
+//heey
+
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(firstName + " " + middleName + " " + lastName);
