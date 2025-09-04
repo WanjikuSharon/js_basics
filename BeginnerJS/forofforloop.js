71d12e41aa856347c79927b1990e4f9d4351e473
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-09-04 10:29:56 +0300

    Add forofforloop.js file with income summation and character iteration examples

---
+++ BeginnerJS/forofforloop.js
@@ -0,0 +1,18 @@
+let incomes = [1000, 2000, 3000, 4000, 5000]
+
+total = 0;
+
+for (const income of incomes) {
+    //print out individual income
+    console.log(income);
+  total += income;
+}
+//print out the total
+console.log(total);
+
+//Get characters in a name or sentences
+let fullName = "My name is Ivy Stephanie Nudngu";
+
+for (const char of fullName) {
+    console.log(char);
+}
