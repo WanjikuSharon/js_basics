35ba7b226493ae9f90e125cc0a6ce00defa3d6c9
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-09-04 10:29:20 +0300

    Remove withinrange.js file and its associated functionality

--- withinrange.js
+++
@@ -1,7 +0,0 @@
-function myRandomRange(myMin, myMax) {
-   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
-}
-
-var myRand = myRandomRange(1, 100);
-
-console.log(myRand); // Example usage
