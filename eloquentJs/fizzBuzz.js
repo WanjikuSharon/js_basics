2f2636b439a591fdb5c98c372374d13f07dc7577
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-09-11 11:49:47 +0300

    Fix FizzBuzz implementation to log the count of iterations

--- eloquentJs/fizzBuzz.js
+++ eloquentJs/fizzBuzz.js
@@ -9,3 +9,15 @@
 // (This is actually an interview question that has been claimed to weed
 // out a significant percentage of programmer candidates. So if you solved
 // it, your labor market value just went up.)
+
+let count = 0;
+for (num = 1; num <=100; num++){
+  count += 1;
+  if (num % 3 === 0){
+    console.log("Fizz")
+    }
+  else if (num % 5 === 0) {
+    console.log("Buzz")
+    }
+    console.log(num);
+  }
