77958259ae0c6e104501e704680a5fa992065d9c
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-09-11 11:52:54 +0300

    Refactor FizzBuzz logic to remove unnecessary console log for numbers not divisible by 3 or 5

--- eloquentJs/fizzBuzz.js
+++ eloquentJs/fizzBuzz.js
@@ -16,8 +16,7 @@ for (num = 1; num <=100; num++){
   if (num % 3 === 0){
     console.log("Fizz")
     }
-  else if (num % 5 === 0) {
+  else if (num % 5 === 0){
     console.log("Buzz")
     }
-    console.log(num);
   }
